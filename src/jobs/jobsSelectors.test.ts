769dabe7a5ed52e8110efd4a2a2ec394f986a1b6
Guillaume Vincent <gvincent@redhat.com>
2022-04-09 05:17:37 +0000

    Introduce pipeline jobs
    Some jobs follow each other.
    It is now possible to see the relationship between jobs in the table view.
    
    Change-Id: I32fa8bfeac9c767d8615ea1bf85fa813f408ad3d

---
+++ src/jobs/jobsSelectors.test.ts
@@ -0,0 +1,66 @@
+import { IEnhancedJob } from "types";
+import { groupJobsByPipeline } from "./jobsSelectors";
+
+test("groupJobByPipeline", () => {
+  const jobs = [
+    {
+      id: "j5",
+      previous_job_id: "j1",
+      created_at: "2018-06-14T15:30:50.139451",
+    },
+    {
+      id: "j4",
+      previous_job_id: null,
+      created_at: "2018-06-14T15:30:40.139451",
+    },
+    {
+      id: "j3",
+      previous_job_id: null,
+      created_at: "2018-06-14T15:30:30.139451",
+    },
+    {
+      id: "j2",
+      previous_job_id: "j1",
+      created_at: "2018-06-14T15:30:20.139451",
+    },
+    {
+      id: "j1",
+      previous_job_id: null,
+      created_at: "2018-06-14T15:30:10.139451",
+    },
+  ] as unknown as IEnhancedJob[];
+  const expectedJobs = [
+    [
+      {
+        id: "j1",
+        previous_job_id: null,
+        created_at: "2018-06-14T15:30:10.139451",
+      },
+      {
+        id: "j5",
+        previous_job_id: "j1",
+        created_at: "2018-06-14T15:30:50.139451",
+      },
+      {
+        id: "j2",
+        previous_job_id: "j1",
+        created_at: "2018-06-14T15:30:20.139451",
+      },
+    ],
+    [
+      {
+        id: "j4",
+        previous_job_id: null,
+        created_at: "2018-06-14T15:30:40.139451",
+      },
+    ],
+    [
+      {
+        id: "j3",
+        previous_job_id: null,
+        created_at: "2018-06-14T15:30:30.139451",
+      },
+    ],
+  ] as unknown as IEnhancedJob[][];
+  expect(groupJobsByPipeline(jobs)).toEqual(expectedJobs);
+});
